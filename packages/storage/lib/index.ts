df17bc4aa0bf219fb99540a76bf9e47f751d5774
Dalton Perkins <contact@daltonp.dev>
2024-07-09 23:43:03 -0400

    added tab storage module

--- packages/storage/lib/index.ts
+++ packages/storage/lib/index.ts
@@ -1,4 +1,5 @@
 import { createStorage, StorageType, type BaseStorage, SessionAccessLevel } from './base';
 import { exampleThemeStorage } from './exampleThemeStorage';
+import { tabGroupsStorage } from './tabStorage';
 
-export { exampleThemeStorage, createStorage, StorageType, SessionAccessLevel, BaseStorage };
+export { exampleThemeStorage, tabGroupsStorage, createStorage, StorageType, SessionAccessLevel, BaseStorage };
