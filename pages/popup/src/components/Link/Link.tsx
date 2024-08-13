0094517948352a67e6d3f12f9c914aa43e9d7431
Dalton Perkins <contact@daltonp.dev>
2024-08-13 11:22:05 -0400

    created link component

---
+++ pages/popup/src/components/Link/Link.tsx
@@ -0,0 +1,29 @@
+import { ReactNode } from 'react';
+
+type LinkProps = {
+  href: string;
+  children: ReactNode;
+};
+
+export const Link = ({ href, children }: LinkProps) => {
+  const handleLinkClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    chrome.tabs.create({ url: href });
+    // e.button === 2
+    console.log(e.button, e);
+  };
+
+  const handleAuxClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    console.log(e.button);
+    // handle middle mouse click
+    if (e.button === 1) {
+      chrome.tabs.create({ url: href, active: false });
+    }
+  };
+
+  return (
+    <a onClick={handleLinkClick} onAuxClick={handleAuxClick}>
+      {children}
+    </a>
+  );
+};
