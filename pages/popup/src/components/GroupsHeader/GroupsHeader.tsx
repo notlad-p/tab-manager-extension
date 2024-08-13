6d4b9502578e26725c69a2d71f58ad8ff64df8e4
Dalton Perkins <contact@daltonp.dev>
2024-08-13 11:21:52 -0400

    created groups header

---
+++ pages/popup/src/components/GroupsHeader/GroupsHeader.tsx
@@ -0,0 +1,88 @@
+import { useStorageSuspense } from '@chrome-extension-boilerplate/shared';
+import { collectionsStorage } from '@chrome-extension-boilerplate/storage';
+import { Popover, Button, TextInput, Checkbox } from '@mantine/core';
+import { useState, FormEvent } from 'react';
+import type { Tab } from '@chrome-extension-boilerplate/storage';
+import { IconPlus } from '@tabler/icons-react';
+
+type GroupsHeaderProps = {
+  activeWindow: Tab[] | null;
+};
+
+export const GroupsHeader = ({ activeWindow }: GroupsHeaderProps) => {
+  const [newGroupOpen, setNewGroupOpen] = useState(false);
+  const [groupName, setGroupName] = useState('');
+  const [addCurrentWindow, setAddCurrentWindow] = useState(false);
+
+  const collections = useStorageSuspense(collectionsStorage);
+  const activeCollection = collections.collections.find(col => col.id === collections.activeCollectionId);
+  const numTabs = activeCollection?.groups.reduce((acc, cur) => acc + cur.tabs.length, 0);
+
+  const handleCreateNewGroup = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setNewGroupOpen(false);
+
+    if (activeWindow) {
+      collectionsStorage.createGroup({
+        collectionId: collections.activeCollectionId,
+        name: groupName,
+        tabs: addCurrentWindow ? activeWindow : [],
+      });
+    }
+  };
+
+  return (
+    <div>
+      <div className="pt-4 flex items-center">
+        <div className="w-3 h-3 rounded-full mr-3" style={{ backgroundColor: activeCollection?.color }} />
+        <h1 className="text-xl font-extrabold">{activeCollection?.name}</h1>
+      </div>
+
+      <div className="pt-2 pb-2 border-b border-stone-700 flex justify-between items-end">
+        <p className="text-stone-400">
+          {activeCollection?.groups.length} Group
+          {activeCollection?.groups && activeCollection?.groups.length > 1 && 's'} - {numTabs} Tab
+          {numTabs && numTabs > 1 && 's'}
+        </p>
+
+        <Popover
+          width={244}
+          opened={newGroupOpen}
+          onChange={setNewGroupOpen}
+          onClose={() => setGroupName('')}
+          trapFocus
+          position="bottom-end"
+          shadow="md"
+        >
+          <Popover.Target>
+            <Button size="xs" rightSection={<IconPlus size={12} />} onClick={() => setNewGroupOpen(o => !o)}>
+              New Group
+            </Button>
+          </Popover.Target>
+          <Popover.Dropdown>
+            <form className="flex flex-col gap-2" onSubmit={handleCreateNewGroup}>
+              <TextInput
+                label="Name"
+                value={groupName}
+                onChange={event => setGroupName(event.currentTarget.value)}
+                placeholder="Group Name"
+                size="xs"
+              />
+              <Checkbox
+                label="Add tabs in current window"
+                size="xs"
+                value={addCurrentWindow}
+                onChange={event => setAddCurrentWindow(event.currentTarget.checked)}
+              />
+              <div className="flex items-end justify-end">
+                <Button type="submit" size="xs">
+                  Create
+                </Button>
+              </div>
+            </form>
+          </Popover.Dropdown>
+        </Popover>
+      </div>
+    </div>
+  );
+};
