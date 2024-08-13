7ac2e2968c18a4081db9534037602c829414c8bd
Dalton Perkins <contact@daltonp.dev>
2024-08-13 11:25:14 -0400

    export components

---
+++ pages/popup/src/components/index.ts
@@ -0,0 +1,4 @@
+export * from './Link/Link';
+export * from './TabGroupsAccordion/TabGroupsAccordion';
+export * from './GroupsHeader/GroupsHeader';
+export * from './CollectionsSidebar/CollectionsSidebar';
